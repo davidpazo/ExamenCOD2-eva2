9f96f7c0e5dbd3fa90091f9805366090c498f1b1
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2015-03-19 09:00:34 +0100

    Configuration Builder listo

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -6,9 +6,11 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Clase para recobrar los twitts del morrazo
+ *
  * @author damian
  */
 public class StatusTwitter {
@@ -18,13 +20,21 @@ public class StatusTwitter {
      * @throws twitter4j.TwitterException
      */
     public static void main(String[] args) throws TwitterException {
-     
+
         Twitter mitwitter = new TwitterFactory().getInstance();
- 
+
         Query query = new Query("#Cangas");
         QueryResult result = mitwitter.search(query);
         for (Status status : result.getTweets()) {
             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
         }
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("9WKYJ6XIVJRVrJVcNIgs5GPFG")
+                .setOAuthConsumerSecret("6UrWbAQORsDXOxP82BgTSuo23f2nQQEr228sFAK5AU0q79VP8o")
+                .setOAuthAccessToken("2258833753-zZzwJ3egxm8TNsOHXfSp17m8GLPnftY2LimQgwu")
+                .setOAuthAccessTokenSecret("VHvUE0RrLrgch9OjnUg8NwU117GciMG8gdZQ30LhBKzDb");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter gonzalo = tf.getInstance();
     }
 }
